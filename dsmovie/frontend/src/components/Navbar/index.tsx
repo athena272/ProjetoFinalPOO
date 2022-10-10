f3deca22c62c7358e7ae9160662cd7ed656fb01f
athena272 <guilhermera272@gmail.com>
2022-10-10 15:06:26 -0300

    Update index.tsx

--- dsmovie/frontend/src/components/Navbar/index.tsx
+++ dsmovie/frontend/src/components/Navbar/index.tsx
@@ -7,10 +7,22 @@ function Navbar(){
         <nav className="container">
             <div className="dsmovie-nav-content">
                 <h1>DSMovie</h1>
+                <a href="https://github.com/alysz" target="_blank" rel="noreferrer">
+                    <div className="dsmovie-contact-container">
+                        <GithubIcon />
+                        <p className="dsmovie-contact-link">/alysz</p>
+                    </div>
+                </a>
                 <a href="https://github.com/athena272" target="_blank" rel="noreferrer">
                     <div className="dsmovie-contact-container">
                         <GithubIcon />
-                        <p className="dsmovie-contact-link">/Big Three</p>
+                        <p className="dsmovie-contact-link">/athena272</p>
+                    </div>
+                </a>
+                <a href="https://github.com/athena272" target="_blank" rel="noreferrer">
+                    <div className="dsmovie-contact-container">
+                        <GithubIcon />
+                        <p className="dsmovie-contact-link">/Vitordotpy</p>
                     </div>
                 </a>
             </div>
